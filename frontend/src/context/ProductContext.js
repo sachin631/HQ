cef5abeb36b411afa5131a948df4bc2e1767e798
sachin <84963232+sachin631@users.noreply.github.com>
2023-04-11 09:02:43 +0530

    Delete frontend directory

--- frontend/src/context/ProductContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react"
-
-const AppContext = createContext();
-
-const AppProvider = ({ children }) => {
-    return <AppContext.Provider value='Hi'>{children}</AppContext.Provider>;
-};
-
-export { AppProvider };
